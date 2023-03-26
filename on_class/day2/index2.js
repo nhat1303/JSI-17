6786fff454c5fb5166e8446f745c96a7de5b559a
nhat1303 <nhathahaha13@gmail.com>
2023-03-26 21:16:31 +0700

    day3

--- on_class/day2/index2.js
+++ on_class/day2/index2.js
@@ -1,3 +1,5 @@
+// https://docs.google.com/document/d/1fASkBnSu4QrvgfnROstlksPFqkS-X9S91cGz0MQQuF8/edit
+
 // BÀI 1
 const arr1 = [1,2,3,4,5]
 const newArray1 = arr1.concat(arr1)
@@ -30,7 +32,82 @@ console.log(arrOdds)
 
 // BÀI 4 
 
-function filterRange(arr, a, b) {
-    return arr.filter(item => (a <= item && item <= b));
+function filterRange1(arr, a, b) {
+    return arr.filter(item => (arr[a] <= item && item <= arr[b]));
 }
-console.log(filterRange([1,2,3,4], 2, 4));
+console.log(filterRange2([2,6,7,1,3], 1, 4));
+
+// BÀI 5 
+function filterRange2(arr, a, b){
+    return arr.filter(x => (x > a && x < b))
+}
+
+console.log(filterRange2([1,2,3,4],2,4))
+
+// BÀI 6 
+
+let student = [
+    {
+        name: "Duong",
+        age : 11,
+        hobbies: ["video games", "sleep", "learn"]
+    },
+    {
+        name: "Dong",
+        age : 12,
+        hobbies: ["video games", "sick", "study"]
+    },
+    {
+        name: "Minh",
+        age : 13,
+        hobbies: ["video games", "sick", "learn"]
+    },
+    {
+        name: "Thanh",
+        age : 9,
+        hobbies: ["play games", "sleep", "learn"]
+    },
+    {
+        name: "Hung",
+        age : 11,
+        hobbies: ["play games", "sick", "learn"]
+    }
+]
+// 1 
+
+let inputAge= prompt("Nhập số tuổi")
+const sdList1 = student.filter((student) => student.age == inputAge);
+console.log(sdList1);
+
+// 2
+
+let inputHobby = prompt("Nhập sở thích")
+for (i = 0; i<3 ; i++){
+    const sdList2 = student.filter((student)=> student.hobbies[i]== inputHobby);
+    console.log(sdList2)
+
+}
+
+// 3
+for (i = 0 ;i <3; i++){
+    const sdList3 = student.filter((student) => student.hobbies[i]=="study")
+    console.log(sdList3)
+}
+
+// 4
+for (i = 0 ; i<3 ; i++){
+    
+    {
+    
+        const sdList4 = student.map(function(student){
+            if (
+                student[i]= 11
+            ){
+                return student.age =15
+            }
+        }  )
+        console.log(sdList4)
+        
+    }
+    
+}
---
+++ on_class/day3/car.js
@@ -0,0 +1,16 @@
+class Car {
+    constructor(brand,speed, color) {
+        this.brand = brand;
+        this.speed = speed;
+        this.color = color
+    }
+    
+}
+
+
+let car_1 = new Car ("Audi", "100", "red")
+console.log(car_1)
+let car_2 = new Car ("BWM", "120", "blue")
+console.log(car_2)
+let car_3 = new Car ("Lambor", "150", "red")
+console.log(car_3)
